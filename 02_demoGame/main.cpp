e5a07797febbe3cf5bf057b8213f7fe223244643
Reski Firmansyah <93902289+ReskiF1@users.noreply.github.com>
2022-06-29 17:45:24 +0700

    Update main.cpp

--- 02_demoGame/main.cpp
+++ 02_demoGame/main.cpp
@@ -1,108 +1,146 @@
+// Naruto Senki
+
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 #define KEY_UP 72
 #define KEY_DOWN 80
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
+#define ESC_KEY 27
 
+// struct property
+struct Karakter {
+// parameter
+	string nama;
+    int hp;
+    int atk;
+    
+    printHP() {
+    	cout << "HP " << nama << " saat ini adalah " << hp << "\n";
+	}
+	
+	void attack(Karakter *heroObject) {
+		int hpFrom = heroObject->hp;
+		cout << nama << " attacking " << heroObject->nama << " ! \n";
+		heroObject->hp = heroObject->hp - atk;
+		cout << heroObject->nama << "'s hp drop from " << hpFrom << " to " << heroObject->hp << "\n";
+	}
+    
+};
+
+// function
 int main(){
-    // Game pokemon
-    /*
-    1. Jalan        :   - arrow key pindah posisi
-    2. Rumput pendek:   - arrow key pindah posisi
-    3. Semak belukar:   - arrow key pindah posisi
-                        - akan muncul pokemon secara random (random 0.0 - 1.0, < 0.3)
-    4. Pohon        :   - arrow key tidak bisa pindah ke area ini
-    5. Batu         :   - arrow key tidak bisa pindah ke area ini
-    6. Genangan air :   - arrow key pindah posisi
-    7. Perairan     :   - arrow key pindah posisi (asal perahu = true) 
-                        - akan muncul pokemon tipe water / flying Ukuran environment (random 0.0 - 1.0, < 0.3)
-    */
     
-    int panjangPeta = 20;	// x
-    int lebarPeta = 16;		// y
-    
-    int posisiKarakterY = 15;
-    int posisiKarakterX = 0;
-    
-    cout << "Karakter ada di y:" << posisiKarakterY << ", x:" << posisiKarakterX << "\n";
-    
-    int peta[lebarPeta][panjangPeta] = {
-        {7, 7, 7, 7, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-        {7, 7, 7, 7, 7, 7, 1, 1, 1, 1, 1, 1, 1, 4, 4, 1, 1, 1, 1, 1},
-        {7, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 1, 1, 1, 1, 1},
-        {7, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 7, 7, 7, 7, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 1, 7, 7, 7, 7, 7, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 1, 1, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 1, 1, 1},
-        {1, 1, 1, 4, 4, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1},
-        {1, 1, 1, 4, 4, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 5, 5, 5, 5},
-        {1, 1, 4, 4, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1},
-        {1, 1, 4, 4, 1, 1, 1, 1, 3, 3, 3, 1, 1, 3, 3, 1, 1, 1, 1, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    Karakter k[] = {
+        {"Naruto",
+         550,
+         20},
+        {"Sasuke",
+         500,
+         25},
     };
     
-    int arrowKey = 0;
+    int panjangPeta = 49;		// x
+    int lebarPeta = 7;			// y
+    int posisiKarakterY = 3;
+    int posisiKarakterX = 7;
     
+// Matrix-2D
+	/*Aturan menggerakkan karakter
+	1. Jalan        (1 / " ")	:   - arrow key pindah posisi
+	2. Healing Zone	(2 / "*")	:   - arrow key pindah posisi
+	  								- dapat tambahan nyawa
+	3. Tower A		(3 / "\")	:   - arrow key tidak bisa tembus
+	4. Tower Gun A	(4 / "A")	:	- arrow key tidak bisa tembus
+									- menghasilkan damage
+	5. Tower B		(5 / "/")	:	- arrow key tidak bisa tembus
+									- harus dihancurkan
+	6. Tower Gun B	(6 / "B")	:	- arrow key tidak bisa tembus
+									- menghasilkan damage
+									- harus dihancurkan
+	7. Rerumputan   (7 / "%")	:   - arrow key pindah posisi
+	8. Tebing		(8 / "#")	:	- arrow key tidak bisa tembus
+	*/
+	
+    // Map
+    int peta[lebarPeta][panjangPeta] = {
+        {8, 8, 8, 8, 8, 8, 1, 7, 7, 7, 1, 1, 1, 1, 1, 7, 7, 7, 1, 1, 1, 1, 1, 7, 7, 7, 1, 1, 1, 1, 1, 7, 7, 7, 1, 1, 1, 1, 1, 7, 7, 7, 1, 8, 8, 8, 8, 8, 8},	//01
+        {8, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 8},	//02
+        {8, 2, 2, 2, 3, 2, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 2, 5, 2, 2, 2, 8},	//03
+        {8, 2, 2, 3, 4, 3, 1, 1, 1, 1, 1, 3, 4, 3, 1, 1, 1, 1, 1, 3, 4, 3, 1, 1, 1, 1, 1, 5, 6, 5, 1, 1, 1, 1, 1, 5, 6, 5, 1, 1, 1, 1, 1, 5, 6, 5, 2, 2, 8},	//04
+        {8, 2, 2, 2, 3, 2, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 2, 5, 2, 2, 2, 8},	//05
+        {8, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 8},	//06
+        {8, 8, 8, 8, 8, 8, 1, 7, 7, 7, 1, 1, 1, 1, 1, 7, 7, 7, 1, 1, 1, 1, 1, 7, 7, 7, 1, 1, 1, 1, 1, 7, 7, 7, 1, 1, 1, 1, 1, 7, 7, 7, 1, 8, 8, 8, 8, 8, 8},	//07
+    };
+	//	01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49
+	
+	// agar pas awal sudah langsung di playground    
+    cout << "8 8 8 8 8 8 1 7 7 7 1 1 1 1 1 7 7 7 1 1 1 1 1 7 7 7 1 1 1 1 1 7 7 7 1 1 1 1 1 7 7 7 1 8 8 8 8 8 8\n"
+    	 << "8 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 8\n"
+         << "8 2 2 2 3 2 1 1 1 1 1 1 3 1 1 1 1 1 1 1 3 1 1 1 1 1 1 1 8 1 1 1 1 1 1 1 8 1 1 1 1 1 1 2 5 2 2 2 8\n"
+         << "8 2 2 3 4 3 1 0 1 1 1 3 4 3 1 1 1 1 1 4 3 3 1 1 1 1 1 8 6 8 1 1 1 1 1 8 6 8 1 1 1 1 1 5 6 5 2 2 8\n"
+         << "8 2 2 2 2 2 1 1 1 1 1 1 3 1 1 1 1 1 1 1 3 1 1 1 1 1 1 1 8 1 1 1 1 1 1 1 8 1 1 1 1 1 1 2 5 2 2 2 8\n"
+         << "8 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 8\n"
+         << "8 8 8 8 8 8 1 7 7 7 1 1 1 1 1 7 7 7 1 1 1 1 1 7 7 7 1 1 1 1 1 7 7 7 1 1 1 1 1 7 7 7 1 8 8 8 8 8 8\n";
+	
+	// Game akan dijalankan lewat keyboard
+	int arrowKey = 0;
+    
+//Looping While
     while(1) {
-        // Input Keyboard
-        cout << "Masukan arrow key ";
-        cin >> arrowKey;
-        cout << "Arrow key yang dimasukan " << arrowKey << "\n";
+    	
+    	// menangkap ketikan keyboard
+        arrowKey = getch();
         
-        // Aturan menggerakkan karakter ke atas
-        if(arrowKey == 72 && (peta[posisiKarakterY-1][posisiKarakterX] == 1
+        if(arrowKey == KEY_UP
+			&& (peta[posisiKarakterY-1][posisiKarakterX] == 1
 			||  peta[posisiKarakterY-1][posisiKarakterX] == 2
-			||  peta[posisiKarakterY-1][posisiKarakterX] == 3
-			||  peta[posisiKarakterY-1][posisiKarakterX] == 6)
-			&& posisiKarakterY >= 0) {
+			||  peta[posisiKarakterY-1][posisiKarakterX] == 7)
+			&& posisiKarakterY >= 0) {						
 			// Gerakkan karakter ke atas
             posisiKarakterY = posisiKarakterY-1;
-        }
-        
-        // Aturan menggerakkan karakter ke bawah
-        if(arrowKey == 80									//Ke bawah kalau input angka 80
+        } else
+        if(arrowKey == KEY_DOWN
 			&& (peta[posisiKarakterY+1][posisiKarakterX] == 1			
 			||  peta[posisiKarakterY+1][posisiKarakterX] == 2
-			||  peta[posisiKarakterY+1][posisiKarakterX] == 3
-			||  peta[posisiKarakterY+1][posisiKarakterX] == 6)
-			&& posisiKarakterY < lebarPeta) {					//Border peta bagian bawah
-            // Gerakkan karakter ke atas
+			||  peta[posisiKarakterY+1][posisiKarakterX] == 7)
+			&& posisiKarakterY < lebarPeta) {
+            // Gerakkan karakter ke bawah
 			posisiKarakterY = posisiKarakterY+1;
-        }
-        
-        // Aturan menggerakkan karakter ke kiri
-        if(arrowKey == 75 
+        } else
+        if(arrowKey == KEY_LEFT 
 			&& (peta[posisiKarakterY][posisiKarakterX-1] == 1
 			||  peta[posisiKarakterY][posisiKarakterX-1] == 2
-			||  peta[posisiKarakterY][posisiKarakterX-1] == 3
-			||  peta[posisiKarakterY][posisiKarakterX-1] == 6) 
+			||  peta[posisiKarakterY][posisiKarakterX-1] == 7) 
 			&& posisiKarakterX >= 0) {
-            // Gerakkan karakter ke atas
+            // Gerakkan karakter ke kiri
             posisiKarakterX = posisiKarakterX-1;
-        }
-        
-        // Aturan menggerakkan karakter ke kanan
-        if(arrowKey == 77 
+        } else
+        if(arrowKey == KEY_RIGHT 
 			&& (peta[posisiKarakterY][posisiKarakterX+1] == 1
 			||  peta[posisiKarakterY][posisiKarakterX+1] == 2
-			||  peta[posisiKarakterY][posisiKarakterX+1] == 3
-			||  peta[posisiKarakterY][posisiKarakterX+1] == 6)
+			||  peta[posisiKarakterY][posisiKarakterX+1] == 7)
 			&& posisiKarakterX < panjangPeta) {
-            // Gerakkan karakter ke atas
+            // Gerakkan karakter ke kanan
             posisiKarakterX = posisiKarakterX+1;
-        }
+        } else
+        if(arrowKey == ESC_KEY){
+        	cout << "Exiting...\n";
+        	break;
+        	// ESC untuk menghentikan permainan
+		};
         
-        // Border Peta
-        for(int y=0; y<lebarPeta; y++) {
-            for(int x=0; x<panjangPeta; x++) {
-                if(posisiKarakterX == x && posisiKarakterY == y) {
-                    cout << 0 << " ";
+        //pembersih output sebelumnya
+        system("cls");
+        
+//Nested For Looping
+		// gerakan karakter
+        for(int y = 0; y < lebarPeta; y++){
+            for(int x = 0; x < panjangPeta; x++){
+                if(posisiKarakterX == x && posisiKarakterY == y){
+					cout << "0" << " ";
                 } else {
                     cout << peta[y][x] << " ";
                 }
@@ -111,5 +149,6 @@ int main(){
         }
     }
     
+    cout << "Exitted";
     return 0;
 }
